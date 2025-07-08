f8acaa1ad47b66467a73acf3fcd7d90d513448e0
Salomao S. <souza95salomao@gmail.com>
2025-07-08 20:52:04 +0000

    :construction: db: setup database scripts - seeding and table t_rooms creation

---
+++ server/src/main/resources/seed.sql
@@ -0,0 +1,7 @@
+--DELETE FROM t_rooms;
+INSERT INTO t_rooms
+  (id, title, description, slug, created_at) VALUES
+  ('1937-128-1838-913838-9138', 'Nlw Agents Room 001', 'Vivamus auctor, nisl sit amet mollis vulputate, elit sem vulputate ante, at scelerisque nibh velit id mauris.', 'nlw-agents-room-001', '2025-07-01T12:45:56Z'),
+  ('2853-284-2847-238380-2733', 'Nlw Agents Room 002', 'Vivamus auctor, nisl sit amet mollis vulputate, elit sem vulputate ante, at scelerisque nibh velit id mauris.', 'nlw-agents-room-002', '2025-07-01T12:46:06Z'),
+  ('0194-283-2847-283883-8383', 'Nlw Agents Room 0038', 'Vivamus auctor, nisl sit amet mollis vulputate, elit sem vulputate ante, at scelerisque nibh velit id mauris.', 'nlw-agents-room-003', '2025-07-01T12:46:32Z'),
+  ('1910-183-2848-283838-2830', 'Nlw Agents Room 004', 'Vivamus auctor, nisl sit amet mollis vulputate, elit sem vulputate ante, at scelerisque nibh velit id mauris.', 'nlw-agents-room-004', '2025-07-01T12:47:16');
