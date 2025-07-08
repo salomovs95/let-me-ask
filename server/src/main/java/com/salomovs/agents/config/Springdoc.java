b8c8b5fbb0d9527ba2bfece950f0481ced30d1a6
Salomao S. <souza95salomao@gmail.com>
2025-07-08 13:47:21 +0000

    :construction: application: setup config - Springdoc/Swagger

---
+++ server/src/main/java/com/salomovs/agents/config/Springdoc.java
@@ -0,0 +1,15 @@
+package com.salomovs.agents.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class Springdoc {
+  public OpenAPI openAPI() {
+    Info info = (new Info()).title("Car Rental API");
+    OpenAPI api = (new OpenAPI()).info(info);
+    return api;
+  }
+}
