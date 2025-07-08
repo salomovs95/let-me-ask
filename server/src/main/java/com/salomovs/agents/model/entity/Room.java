def4ad2b1288970a6a156207ed365602296cffda
Salomao S. <souza95salomao@gmail.com>
2025-07-08 13:45:42 +0000

    :construction: db: setup entities - Room

---
+++ server/src/main/java/com/salomovs/agents/model/entity/Room.java
@@ -0,0 +1,44 @@
+
+package com.salomovs.agents.model.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="t_rooms")
+public class Room {
+  @Id
+  @GeneratedValue(strategy=GenerationType.UUID)
+  private String id;
+
+  @Column(nullable=false, unique=true)
+  private String slug;
+
+  @Column(nullable=false)
+  private String title;
+
+  private String description;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+
+  public Room(String title, String description) {
+    this.title = title;
+    this.description = description;
+    this.slug = title.replace(" ", "-").toLowerCase();
+  }
+}
