77055873370c18dfab63e6c78b9978343a1e878e
Salomao S. <souza95salomao@gmail.com>
2025-07-10 12:06:35 +0000

    :construction: web: update rooms listing

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,29 +1,13 @@
-import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-
-import { useFetch } from '@/http/use-fetch'
-
-type Room = {
-  slug: String,
-  title: String
-}
-
-type GetRoomResponse = Array<Room>
+import { RoomList } from '@/components/room-list'
 
 export function CreateRoom() {
-  //const API_URL = `${import.meta.env.VITE_API_URL}/rooms/`
-  const { isLoading, data, error } = useFetch<GetRoomResponse>('/rooms/', 'rooms_list')
-
   return (
-    <>
-      {error && <p className="text-red-600">{error.message}</p>}
-      {isLoading && <p>Loading...</p>}
-      {data?.map((room:Room)=>(
-        <Link key={room.slug} to={`/rooms`}>
-          <span>{room.title}</span>
-          <span>entrar →</span>
-        </Link>
-      ))}
-    </>
+    <div className="min-h-screen px-4 py-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid grid-cols-2 items-start gap-8">
+          <RoomList />
+        </div>
+      </div>
+    </div>
   )
 }
