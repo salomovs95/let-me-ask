dd8999a3e52bab94cc2747ac7c2569ddc0921f01
Salomao S. <souza95salomao@gmail.com>
2025-07-10 12:02:34 +0000

    :construction: web: update rooms listing

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -4,7 +4,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { CreateRoom } from '@/pages/create-room'
-import { Room } from '@/pages/room'
 
 const client = new QueryClient()
 export function App() {
@@ -13,7 +12,6 @@ export function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<CreateRoom />} index />
-          <Route element={<Room />} path="/rooms/:slug" />
         </Routes>
       </BrowserRouter>
     </ QueryClientProvider>
